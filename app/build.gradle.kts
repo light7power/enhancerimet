9c94e84dab22382600b1da626956fc529045f13c
light7power <light7power@users.noreply.github.com>
2026-09-15 15:27:16 +0800

    ci: publish APK release and GitHub package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("maven-publish")
 }
 
 android {
@@ -28,4 +29,29 @@ android {
 dependencies {
     compileOnly("io.github.libxposed:api:102.0.0")
     implementation(files("libs/service-classes.jar", "libs/interface-classes.jar"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "rimet.enhancement"
+            artifactId = "modern"
+            version = "2.0.1"
+            artifact(layout.buildDirectory.file("outputs/apk/release/app-release-unsigned.apk"))
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/light7power/enhancerimet")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+tasks.named("publish") {
+    dependsOn("assembleRelease")
 }
